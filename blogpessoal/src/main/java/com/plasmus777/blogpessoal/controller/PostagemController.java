d4b8e79d5d49ae8e0c1b44fb6374266b418b732c
Fernando Lopes <118869201+plasmus777@users.noreply.github.com>
2024-01-11 16:22:52 -0300

    Create PostagemController.java

---
+++ blogpessoal/src/main/java/com/plasmus777/blogpessoal/controller/PostagemController.java
@@ -0,0 +1,27 @@
+package com.plasmus777.blogpessoal.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.plasmus777.blogpessoal.model.Postagem;
+import com.plasmus777.blogpessoal.repository.PostagemRepository;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class PostagemController {
+	
+	@Autowired
+	private PostagemRepository postagemRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Postagem>> getAll(){
+		return ResponseEntity.ok(postagemRepository.findAll());
+	}
+}
