a7b76a57396eefdea7ff74c275cba7616a3c27f0
Fernando Lopes <118869201+plasmus777@users.noreply.github.com>
2024-01-17 12:35:37 -0300

    Criação do projeto Loja de Jogos

---
+++ lojaGames/src/test/java/com/plasmus777/lojaGames/LojaGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plasmus777.lojaGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
